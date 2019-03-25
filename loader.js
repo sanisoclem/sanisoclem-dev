85e3ba55236c4f8a2345c4fd5bd2d1640ee3d7cd
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-03-26 02:08:28 +0800

    migrate bootstrap to materialize migrate stylus to sass

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
-import './src/assets/css/main.styl';
+import './src/assets/css/main.scss';
+import 'materialize-css';
 import { setup_nav }  from './src/assets/js/nav.js';
-
 
 // -- jquery nav setup
 setup_nav();
