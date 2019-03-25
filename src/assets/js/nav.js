d02ec90fbcc03f1ab5047680ad421b6c6b6a6580
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-03-26 02:21:53 +0800

    webp fallback

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -20,31 +20,4 @@ export function setup_nav()  {
       }
     });
   });
-
-
-  // // TODO: publish message to rust, represent collapse in rust state
-  // $(window).scroll(() => {
-  //   if ($(".navbar").offset().top > 50) {
-  //       $(".navbar-fixed-top").addClass("top-nav-collapse");
-  //   } else {
-  //       $(".navbar-fixed-top").removeClass("top-nav-collapse");
-  //   }
-  // });
-
-  // // TODO: remove, will be handled in rust
-  // $(()=> {
-  //   const nav = $('.navbar-collapse');
-  //   const toggle = $('navbar-toggle');
-  //   $('.page-scroll a').bind('click', function(event) {
-  //       const $anchor = $(this);
-  //       $('html, body').stop().animate({
-  //           scrollTop: $($anchor.attr('href')).offset().top
-  //       }, 1000, 'easeInOutExpo');
-
-  //       // -- hack because .data-toggle is lame
-  //       nav.removeClass('in').addClass('collapse').css('height','1px');
-  //       toggle.addClass('collapsed');
-  //       event.preventDefault();
-  //   });
-  // });
 }
