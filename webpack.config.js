99847054a98096b5510d4a353442b9df1e170309
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-04-01 18:44:07 +0800

    refactor: add eslint and fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -6,51 +6,51 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./loader.js",
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
-  },
-  mode: "development",
-  module: {
-    rules: [
-      // no need to use babel to transpile to es2015 since async imports and wasm is not supported on old browsers anyway
-      {
-        test: /\.s(c|a)ss$/,
-        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|jxr|webp)$/i,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(html)$/,
-        use: [{
-          loader: 'html-loader',
-          options: {
-            attrs: [':data-src']
-          }
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      hash: true,
-      template: './src/error.html',
-      filename: 'error.html'
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      hash: true,
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-    new CleanWebpackPlugin(),
-    new WebpackMd5Hash()
-  ],
+    entry: "./loader.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    mode: "development",
+    module: {
+        rules: [
+            // no need to use babel to transpile to es2015 since async imports and wasm is not supported on old browsers anyway
+            {
+                test: /\.s(c|a)ss$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|jxr|webp)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(html)$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            template: './src/error.html',
+            filename: 'error.html'
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new CleanWebpackPlugin(),
+        new WebpackMd5Hash()
+    ],
 };
