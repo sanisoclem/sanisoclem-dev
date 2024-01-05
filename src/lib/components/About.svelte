beb6202b7f69a52a5783297805dee8f199b54697
Jerahmeel Cosinas <dev@jerahmeelcosinas.net>
2024-01-05 17:21:02 +1100

    updated text, link to blog

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -5,22 +5,21 @@
 <div class="mx-auto px-8 py-2 max-w-screen-md">
 	<h2 class="text-4xl uppercase mb-16">About</h2>
 	<p class="text-xl mb-8 text-justify">
-		Hello, I'm Jerahmeel. I write code for a living and for fun. Nowadays, I code mostly in F#, C# and
-		Typescript for my day job.
+		Hello, I'm Jerahmeel. I write code for a living and for fun. Nowadays, I code mostly in F#, C#
+		and Typescript for my day job.
 	</p>
 	<p class="text-lg mb-8 text-justify">
-		When I have spare time, I also like to tinker with other technologies too. At the moment, I like <a
+		On my free time, I like to tinker with other technologies. At the moment, I like <a
 			class="underline hover:text-amber-500"
 			href="https://www.rust-lang.org/">rust</a
-		>,
-		<a class="underline hover:text-amber-500" href="https://www.haskell.org/">haskell</a>, and most
-		recently
-		<a class="underline hover:text-amber-500" href="https://rescript-lang.org/">rescript</a>.
+		>
+		and
+		<a class="underline hover:text-amber-500" href="https://www.purescript.org/">purescript</a>.
 	</p>
 	<p class="text-md text-justify">TL;DR: I like technology and I like to build stuff!</p>
 
 	<div class="text-gray-900 text-opacity-50">
-		<GoDown anchor="projects" />
+		<GoDown anchor="contact" />
 	</div>
 </div>
 
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -44,8 +44,25 @@
 
 			<nav class="hidden md:flex items-center gap-x-4">
 				<InPageLink href="about" title="About" active={aboutActive} />
-				<InPageLink href="projects" title="Projects" active={projectsActive} />
 				<InPageLink href="contact" title="Contact" active={contactActive} />
+				<a
+					class="leading-10 p-2 transition uppercase hover:text-amber-500 border-b-4 border-transparent flex gap-x-2 justify-center items-center"
+					href="https://busstop.dev"
+					>Blog <svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-4 h-4"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+						/>
+					</svg>
+				</a>
 			</nav>
 		</div>
 	</div>
